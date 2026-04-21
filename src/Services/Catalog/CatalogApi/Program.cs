e82ddfd32403b8b2b02a3c5b348b42af636020ad
Amr Negm <Amr.Negm@codelabsys.com>
2026-04-21 11:26:11 +0200

    added behavior validation (mediatR pipline ability)

--- src/Services/Catalog/CatalogApi/Program.cs
+++ src/Services/Catalog/CatalogApi/Program.cs
@@ -1,15 +1,22 @@
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // add service here DI
 builder.Services.AddCarter();
+
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 
 
 var app = builder.Build();
